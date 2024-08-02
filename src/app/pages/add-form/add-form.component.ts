ab635260289168f0d599947c1f9f5a6060c37433
omar marrakchi <marrakchi.omar@esprit.tn>
2024-08-02 20:07:58 +0100

    ajout radio dans form

--- src/app/pages/add-form/add-form.component.ts
+++ src/app/pages/add-form/add-form.component.ts
@@ -1,27 +1,39 @@
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import { MatIcon } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { MatRadioButton } from '@angular/material/radio';
 
 @Component({
   selector: 'app-add-form',
   standalone: true,
-  imports: [MatCardModule, FormsModule, MatFormFieldModule, MatInputModule, MatIcon,CommonModule,],
+  imports: [
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    CommonModule,
+    MatRadioButton
+  ],
   templateUrl: './add-form.component.html',
-  styleUrl: './add-form.component.scss'
+  styleUrls: ['./add-form.component.scss']
 })
 export class AddFormComponent {
-  questions: { text: string }[] = [];
-
+  questions: { text: string; options: string[]; answer: string }[] = [];
 
   addQuestion() {
-    this.questions.push({ text: '' });
+    this.questions.push({ text: '', options: ['Option 1'], answer: '' });
   }
 
   removeQuestion(index: number) {
     this.questions.splice(index, 1);
+  }
+
+  addOption(questionIndex: number) {
+    this.questions[questionIndex].options.push('New Option');
   }
 }
