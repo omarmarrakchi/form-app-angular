3f7f19f1d85b8f3f4c092a58a8ef557b8e362d45
omar marrakchi <marrakchi.omar@esprit.tn>
2024-08-03 19:25:41 +0100

    ui add form completed

--- src/app/pages/add-form/add-form.component.ts
+++ src/app/pages/add-form/add-form.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { MatInput, MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatOption, MatSelect, MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-add-form',
@@ -20,13 +21,19 @@ import { MatOption, MatSelect, MatSelectModule } from '@angular/material/select'
     CommonModule,
     MatRadioModule,
     MatOption,
-    MatSelectModule
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInput
   ],
   templateUrl: './add-form.component.html',
   styleUrls: ['./add-form.component.scss']
 })
 export class AddFormComponent {
-  questions: { text: string; options: string[]; answer: string, type: 'radio' }[] = [];
+
+  formTitle: string = '';
+  formDescription: string = '';
+  questions: { text: string; options: string[]; answer: string, type: 'radio' | 'input' }[] = [];
 
   addQuestion() {
     this.questions.push({ text: '', options: ['Option 1'], answer: '', type: 'radio' });
@@ -47,4 +54,15 @@ export class AddFormComponent {
   trackByIndex(index: number, item: any): number {
     return index;
   }
+
+  onSubmit() {
+    console.log('Form Title:', this.formTitle);
+    console.log('Form Description:', this.formDescription);
+    this.questions.forEach((question, index) => {
+      console.log(`Question ${index + 1}:`, question.text);
+      console.log(`Type:`, question.type);
+      console.log(`Options:`, question.options);
+    });
+  }
+
 }
